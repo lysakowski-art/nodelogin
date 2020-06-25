17f9413af147147c93b1eaa93157d04309072f2b
lysakowski-art <lysakowski.michal@gmail.com>
2020-06-25 17:44:15 +0200

    login system done

--- index.js
+++ index.js
@@ -1,83 +1,57 @@
-// const mysql = require("mysql");
+const favicon = null;
+const mysql = require("mysql");
 const express = require("express");
-// const request = require("request");
-// const session = require("express-session");
-// const bodyParser = require("body-parser");
-// const path = require("path");
-const port = 8000;
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "HighPass2020!%",
-//   database: "nodelogin",
-// });
+const port = 8000;
 
 const app = express();
-// app.use(
-//   session({
-//     secret: "secret",
-//     resave: true,
-//     saveUninitialized: true,
-//   })
-// );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// app.get("/", (request, response) => {
-//   response.sendFile(path.join(__dirname + "/login.html"));
-// });
-
-// app.post("/auth", (request, response) => {
-//   const username = request.body.username;
-//   const password = request.body.password;
-//   if (username && password) {
-//     connection.query(
-//       "SELECT * FROM accounts WHERE username = ? AND password = ?",
-//       [username, password],
-//       (error, results, fields) => {
-//         if (results.length > 0) {
-//           request.session.loggedin = true;
-//           request.session.username = username;
-//           response.redirect("/home");
-//         } else {
-//           response.send("Incorrect Username and/or Password!");
-//         }
-//         response.end();
-//       }
-//     );
-//   } else {
-//     response.send("Please enter Username and Password!");
-//     response.end();
-//   }
-// });
-
-// app.get("/home", (request, response) => {
-//   if (request.session.loggedin) {
-//     response.send("Welcome back, " + request.session.username + "!");
-//   } else {
-//     response.send("Please login to view this page!");
-//   }
-//   response.end();
-// });
-
-// app.get(`/auth`, (req, res) => {
-//   res.send("now you are in authorization");
-// });
-// app.get(`/`, (req, res) => {
-//   res.send("hello");
-// });
-
-app.get(`/someWebsite`, (req, res) => {
-  const someValues = [
-    { id: 1, name: "John", lastName: "Doe" },
-    { id: 2, name: "Tim", lastName: "Wining" },
-    { id: 3, name: "Edward", lastName: "Blaze" },
-  ];
-  res.json(someValues);
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "HighPass2020!%",
+  database: "nodelogin",
 });
 
 app.use(cors());
+
+// app.get(`/auth`, (req, res) => {
+//   res.end("CHYBA DZIAŁA");
+// });
+
+app.post("/auth", (req, res) => {
+  const { login, password } = req.body;
+  if (login && password) {
+    connection.query(
+      "SELECT * FROM accounts WHERE username = ? AND password = ?",
+      [login, password],
+      (error, results, fields) => {
+        if (results.length > 0) {
+          req.session.loggedin = true;
+          req.session.username = login;
+          // res.redirect("/home");
+          console.log("działa");
+        } else {
+          res.send(`Incorrect location`);
+        }
+        res.end();
+      }
+    );
+  } else {
+    res.send("Please insert correct values");
+    res.end();
+  }
+});
+
 app.listen(port);
